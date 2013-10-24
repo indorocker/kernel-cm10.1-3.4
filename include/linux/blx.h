2106ead598376473402bfa433eae8fa85df8ecdc
indorocker <mustikajjah@gmail.com>
2013-10-24 12:49:05 +0700

    Fix error module in folder /include/linux/blx.h

--- include/linux/blx.h
+++ include/linux/blx.h
@@ -1,10 +1,11 @@
-#ifdef CONFIG_BLX
-        // check Battery Life Extender charging limit
-        if (msm_batt_blx_charging_limit_reached())
-        {
-                // Battery Life Extender charging limit reached !
-                pr_info("[BATT] %s: Battery Life eXtender - Charging limit reached, no need to start recharging! (capacity=%d, voltage=%d)\n",
-                        __func__, msm_batt_info.batt_capacity, msm_batt_info.battery_voltage);
-                return 0;
-        }
+/* include/linux/blx.h */
+
+#ifndef _LINUX_BLX_H
+#define _LINUX_BLX_H
+
+// for kernels including the 100% charging 'fix' change this to 100:
+#define MAX_CHARGINGLIMIT 100
+
+int get_charginglimit(void);
+
 #endif
